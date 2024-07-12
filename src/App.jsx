e1136617b988f781c12eae7e92f5b4d4444e470d
Caroline de Moraes <109109668+carolinedemoraes@users.noreply.github.com>
2024-07-12 17:33:36 +0100

    code updates-second commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,41 +8,34 @@ function App() {
   const [secondNumber, setSecondNumber] = useState("");
 
   function addNumber(n) {
-    let num = firstNumber + n;
-    setFirstNumber(num);
+    setFirstNumber((prev) => prev + n);
   }
 
   function count(operator) {
-    if (firstNumber != "" && secondNumber != "") {
-      if (operator === "/") {
-        setSecondNumber(parseFloat(secondNumber) / parseFloat(firstNumber));
-      } else if (operator === "*") {
-        setSecondNumber(parseFloat(secondNumber) * parseFloat(firstNumber));
-      } else if (operator === "-") {
-        setSecondNumber(parseFloat(secondNumber) - parseFloat(firstNumber));
-      } else if (operator === "+") {
-        setSecondNumber(parseFloat(secondNumber) + parseFloat(firstNumber));
-      }
-      setOperator(operator);
-      setFirstNumber("");
-    } else {
-      setOperator(operator);
-      setSecondNumber(firstNumber);
-      setFirstNumber("");
+    if (firstNumber !== "" && secondNumber !== "") {
+      result();
     }
+    setOperator(operator);
+    setSecondNumber(firstNumber);
+    setFirstNumber("");
   }
 
   function result() {
-    if (firstNumber != "" && secondNumber != "" && operator != "=") {
-      if (operator === "/") {
-        setFirstNumber(parseFloat(secondNumber) / parseFloat(firstNumber));
+    if (firstNumber !== "" && secondNumber !== "" && operator !== "=") {
+      let result;
+      if (operator === "/" && firstNumber === "0") {
+        alert("Cannot divide by zero");
+        return;
+      } else if (operator === "/") {
+        result = parseFloat(secondNumber) / parseFloat(firstNumber);
       } else if (operator === "*") {
-        setFirstNumber(parseFloat(secondNumber) * parseFloat(firstNumber));
+        result = parseFloat(secondNumber) * parseFloat(firstNumber);
       } else if (operator === "-") {
-        setFirstNumber(parseFloat(secondNumber) - parseFloat(firstNumber));
+        result = parseFloat(secondNumber) - parseFloat(firstNumber);
       } else if (operator === "+") {
-        setFirstNumber(parseFloat(secondNumber) + parseFloat(firstNumber));
+        result = parseFloat(secondNumber) + parseFloat(firstNumber);
       }
+      setFirstNumber(result.toString());
       setSecondNumber("");
       setOperator("=");
     }
@@ -59,16 +52,16 @@ function App() {
   }
 
   function percent() {
-    let percentage = (parseFloat(firstNumber) / 100) * parseFloat(secondNumber);
-    setFirstNumber(percentage);
+    const percentage =
+      (parseFloat(firstNumber) / 100) * parseFloat(secondNumber);
+    setFirstNumber(percentage.toString());
   }
 
   function addDot() {
     if (firstNumber === "") {
       setFirstNumber("0.");
     } else if (!firstNumber.includes(".")) {
-      let num = firstNumber + ".";
-      setFirstNumber(num);
+      setFirstNumber((prev) => prev + ".");
     }
   }
 
@@ -84,9 +77,9 @@ function App() {
 
         <div className="buttons">
           <div className="lines">
-            <Button text={"CE"} onClick={() => cleanEverything("")} />
-            <Button text={"C"} onClick={() => clean("")} />
-            <Button text={"%"} onClick={() => percent()} />
+            <Button text={"CE"} onClick={cleanEverything} />
+            <Button text={"C"} onClick={clean} />
+            <Button text={"%"} onClick={percent} />
             <Button text={"/"} onClick={() => count("/")} />
           </div>
           <div className="lines">
@@ -109,8 +102,8 @@ function App() {
           </div>
           <div className="lines">
             <Button text={"0"} onClick={() => addNumber(0)} />
-            <Button text={"."} onClick={() => addDot()} />
-            <Button text={"="} onClick={() => result("")} />
+            <Button text={"."} onClick={addDot} />
+            <Button text={"="} onClick={result} />
           </div>
         </div>
       </div>
